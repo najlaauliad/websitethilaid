3020ae79a99fc7364c13db403060c53ea173669c
najlaauliad <najlaauliad26@gmail.com>
2021-04-17 20:16:13 +0700

    web

---
+++ mtechno/application/views/v_about.php
@@ -0,0 +1,205 @@
+
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Najla Aulia Dewi" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/Untitled1.png'?>">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="Untitled1.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">THILA.ID<span>.</span></a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Produk</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Blog</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/slide_2a.jpg'?>);">
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>About Us</h2>
+		   					
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div class="fh5co-about animate-box">
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>About Us</h2>
+			<p>Thila.id merupakan perusahaan yang bergerak dan memfokuskan diri pada bidang konfeksi pakaian dan fashion untuk wanita. Thila.id hadir sejak tahun 2017 dengan membuka outlet di Kota Tasikmalaya. Kini Thila.id sudah berkembang dengan membuka beberapa cabang outlet di beberapa kota lainnya. Seiring dengan pesatnya perkembangan teknologi dan keterkaitan nya dengan bidang usaha ini maka kami hadir untuk memberikan produk yang menjadi kebutuhan sehari hari masyarakat saat ini. Kami melayani dengan sepenuh hati secara online maupun offline di outlet kami.</p>
+		</div>
+		<div class="container">
+			<div class="col-md-6">
+				<figure>
+					<img src="<?php echo base_url().'theme/images/image_1.jpeg'?>" alt="Free HTML5 Template" class="img-responsive">
+				</figure>
+			</div>
+			<div class="col-md-6">
+				<h3>Visi</h3>
+				<ul>
+					<li>Menjadi Brand Fashion dengan kenyamanan customer sebagai tujuan utama.</li>
+					<li>Memberikan Layanan dan Solusi untuk dunia fashion saat ini.</li>
+				</ul>
+				<h3>Misi</h3>
+				<ul>
+					<li>Tidak hanya memberi solusi, kami memberikan layanan yang optimal dalam memberikan brand fashion ini dengan menyediakan layanan secara online dan offline.</li>
+					<li>Memberikan produk dan layanan yang berkualitas dan prima bagi customer.</li>
+					<p></p>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>Store</h2>
+			<p>Thila.id sudah hadir di beberapa kota di Indonesia</p>
+			<p>Tasikmalaya, Bandung, Bekasi, Jakarta, Tangerang, Yogyakarta, Surabaya, dan Malang</p>
+		</div>
+	</div>
+
+	
+
+
+
+	<div class="fh5co-team animate-box">
+		<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<h2>PIMPINAN THILA.ID</h2>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person2.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Rostiani</h3>
+						<h4>Co-Founder, CFO</h4>
+						<p>Rostiani merupakan lulusan akuntasi dari perguruan tinggi yang ada di daerah Yogyakarta</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="https://www.instagram.com/rostiani_tian/"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person3.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Asep Rahmadiana</h3>
+						<h4>Founder, CEO</h4>
+						<p>Asep Rahmadiana merupakan seorang pebinis asal Tasikmalaya. Asep R lulusan bisnis manajemen dari perguruan tinggi yang ada di daerah Bandung</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="https://www.instagram.com/aseprahmadiana/"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person4.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Jeni Nugraha</h3>
+						<h4>Co-Founder, CTO</h4>
+						<p>Jeni Nugraha merupakan lulusan teknologi informasi dari perguruan tinggi di Bandung. </p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+	</div>
+
+
+	<div class="fh5co-services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>BRAND</h2>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/baju.jpg'?>" class="img-responsive"></span>
+					<h2>CLOTHES</h2>
+					<p>Atasan sehari hari yang membuat style lebih casual dan kece </p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">See Our Product <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/rok.png'?>" class="img-responsive"></span>
+					<h2>SKIRT</h2>
+					<p>Rok simple yang membuat style fashion lebih feminim dengan bahan yang berkualitas premium</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">See Our Produc <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/celana.png'?>" class="img-responsive"></span>
+					<h2>PANTS</h2>
+					<p>Celana yang dipastikan membuat hari hari mu nyaman memakainya</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">See Our Produc <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
---
+++ mtechno/application/views/v_footer.php
@@ -0,0 +1,43 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				<p>Thila.id merupakan perusahaan yang bergerak dan memfokuskan diri pada bidang konfeksi pakaian dan fashion untuk wanita.</p>
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Information</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'About'?>">About Us</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Our Product</a></li>
+					<li><a href="<?php echo base_url().'Blog'?>">Blog</a></li>
+					<li><a href="<?php echo base_url().'kontak'?>">Contact Us</a></li>
+				</ul>
+
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Product</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Clothes</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Skirt</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Pants</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2020 by <a href="https://www.instagram.com/najlaauliad/" target="_blank">Najla' Aulia Dewi</a>. All Rights Reserved.</p>
+			</div>
+
+		</div>
+	</footer>
